e8b9f2d29069ba67f52a5b7ee09e5f1ccdd53ac7
matheussneves <matheussneves@hotmail.com>
2023-09-13 17:43:41 -0300

    Subindo projeto feito na Alura
    Projeto de teste automatizado com js e cypress.

---
+++ cypress/e2e/api-alurapic.cy.js
@@ -0,0 +1,28 @@
+describe('Api Alurapic', () => {
+    // it('Dados da API', () => {
+    //   cy.request({
+    //     method:'POST',
+    //     url: 'http://localhost:3000/user/login',
+    //     body: Cypress.env()
+    //   }).then((res) => {
+    //     expect(res.status).to.be.equal(200)
+    //     expect(res.body).is.not.empty
+    //     expect(res.body).to.have.property('id')
+    //     expect(res.body.id).to.be.equal(4)
+    //   })
+    
+      it('Fotos do usuário', () => {
+        const tempoEsperado = Math.random() * 50;
+
+        cy.request({
+          method:'GET',
+          url: 'http://localhost:3000/matheus/photos', 
+        }).then((res) => {
+          expect(res.status).to.be.equal(200)
+          expect(res.body).is.not.empty
+          expect(res.body[0]).to.have.property('description')
+          expect(res.body[0].description).to.be.equal('porquinho')  
+          expect(res.duration).to.be.lte(tempoEsperado)       
+        })
+    })
+  })
---
+++ cypress/e2e/login-incorreto.cy.js
@@ -0,0 +1,22 @@
+describe('Página de login', () => {
+
+    beforeEach(() => {
+        cy.visit('http://localhost:4200')
+
+        cy.intercept('POST', 'http://localhost:3000/user/login', {
+            statusCode: 400
+        }).as('stubPost')
+    })
+
+        it('Verifica mensagens de campos obrigatórios na página de login', () => {
+            cy.contains('User name is required!').should('be.visible');
+            cy.contains('Password is required!').should('be.visible');
+        
+        })
+
+        it('Deve falhar mesmo que os campos sejam preenchidos corretamente', () => {
+            cy.login('matheus', '12345678');
+            cy.wait('@stubPost')
+        
+        })
+})
